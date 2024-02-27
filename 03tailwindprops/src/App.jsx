4508646754df288fa7a1478cb6421980a8b0408d
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-02-27 17:18:25 +0530

    Learned about props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+ 
+  return (
+    <>
+      <h1 className='bg-green-400 text-black
+      p-4 rounded-xl'
+      >tailwind test</h1>
+      <Card username = "Chaiaurcode"/>
+      <Card username = "Chaiaurreact"/>
+    </>
+  )
+}
+
+export default App
