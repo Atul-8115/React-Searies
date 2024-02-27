751a8c3dec65a16ad032899220f81f0ffb50159a
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-02-27 15:05:27 +0530

    Learned about hooks

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+
+function App() {
+
+  let [counter, setCounter] = useState(5)
+
+
+  const addValue = () => {
+    if(counter >= 27) {
+      return
+    }
+    console.log("clicked", counter);
+    counter = counter+1;
+    setCounter(counter)
+  }
+
+  const removeValue = () => {
+    if(counter <= 0) {
+      return;
+    }
+    setCounter(counter-1);
+  }
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button 
+      onClick = {addValue}>
+      Add value</button>
+      <button
+      onClick={removeValue}>
+      Decrease value</button>
+    </>
+  )
+}
+
+export default App
